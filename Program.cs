44569c42842f73ced3eb927dcccbc5e3c1cf6a48
JLamzon <jlamzon@yahoo.com>
2024-03-13 10:49:26 -0700

    Day3-DBSetup&Strings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Lvl3Week3Day2_BlogBackend.Services;
+using Lvl3Week3Day2_BlogBackend.Services.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<BlogService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<PasswordService>();
+
+//this is how we’re connecting our database to API
+var connectionString = builder.Configuration.GetConnectionString("MyBlogString");
+
+//configures entity framework core to use SQL server as the database provider for  a datacontext DbContext in our project
+builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(connectionString));
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
